002da4f25249689d34faa13547e43c3f2ab53b49
Elvijs <s22leiselvi@venta.lv>
2025-11-23 23:06:44 +0200

    Add: HomeController, layout, index, routing file.

---
+++ src/resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Project 2 - {{ $title }}</title>
+    <meta name="description" content="My Second Project">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
