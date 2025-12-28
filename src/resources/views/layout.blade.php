ef0909146633b34d90ecd2bb9d898416b80a0be4
Elvijs <s22leiselvi@venta.lv>
2025-12-28 20:09:42 +0200

    Created homepage with Boostrap

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -1,15 +1,37 @@
 <!doctype html>
-<html lang="en">
+<html lang="lv">
 
 <head>
     <meta charset="utf-8">
-    <title>Project 2 - {{ $title }}</title>
-    <meta name="description" content="My Second Project">
+    <title>RSDB - {{ $title }}</title>
+    <meta name="description" content="Web Technologies, 2nd Practical Assignment">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
-    @yield('content')
+    <nav class="navbar bg-primary mb-3" data-bs-theme="dark">
+        <header class="container">
+            <a class="navbar-brand" href="#">Runescape DB - {{ $title }}</a>
+        </header>
+    </nav>
+    <main class="container">
+        <div class="row">
+            <div class="col">
+                @yield('content')
+            </div>
+        </div>
+    </main>
+    <footer class="text-bg-dark mt-3">
+        <div class="container">
+            <div class="row py-5">
+                <div class="col">
+                    Elvijs Leistmanis, 2025
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
